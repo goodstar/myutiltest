0dabc291203ba504cf5d2db5b521eb3d9295b70f
lx15 <lx15@meitu.com>
2018-04-14 11:15:27 +0800

    项目初始化

---
+++ myutiltest-biz/src/main/java/org/myutiltest/biz/App.java
@@ -0,0 +1,13 @@
+package org.myutiltest.biz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
